d890a326caeed0a0c98970c941ad52bb45e71ccb
HoSoo Lee <hosoo.lee@arithnea.de>
2020-11-14 22:29:37 +0100

    11. User Registration

--- config/db.js
+++ config/db.js
@@ -1,18 +1,17 @@
-const mongoose = require('mongoose');
-const config  = require('config');
-const db = config.get('mongoURI');
-
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {useNewUrlParser: true});
+  try {
+    await mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true });
 
-        console.log('MongoDB Connected...')
-    } catch (err) {
-        console.error(err.message);
-        // Exit process with failue
-        process.exit(1);
-    }
-}
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failue
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,27 +1,62 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check')
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator/check");
+
+const User = require("../../models/User");
 
 // @route   POST api/posts
 // @desc    Register route
 // @access  Public
 router.post(
-    '/', 
-    [
-        check('name', "Name is required")
-            .not()
-            .isEmpty(), 
-        check('email', 'please include a valid email').isEmail(),
-        check(
-            'password', 'please enter a password with 6 or more characters'
-            ).isLength({ min: 6})
-], 
-(req, res) => {
-const errors = validationResult(req);
-if(!errors.isEmpty()){
-    return res.status(400).json({errors: errors.array()});
-}    
-res.send('User route')
-});
+  "/",
+  [
+    check("name", "Name is required").not().isEmpty(),
+    check("email", "please include a valid email").isEmail(),
+    check(
+      "password",
+      "please enter a password with 6 or more characters"
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      const avatar = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+
+      res.send("User registered");
+    } catch (err) {
+      console.error(err.message);
+      res.statua(500).send("Server error");
+    }
+  }
+);
 
 module.exports = router;
